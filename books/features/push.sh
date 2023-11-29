2640f32fd74d0c77ac7650739896a6d3a82f9292
kevinge7 <yuxig5@bu.edu>
2023-11-29 12:39:53 -0500

    2023-11-29 12:39:53

--- books/features/push.sh
+++ books/features/push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+commit_message = $1
+
+if [ -z "$commit_message" ]; then
+    commit_message=$(date "+%Y-%m-%d %H:%M:%S")
+fi
+
+
 git add .
-formatted_date=$(date "+%Y-%m-%d %H:%M:%S")
-git commit -m "$formatted_date"
+git commit -m "$commit_message"
 git push --force
